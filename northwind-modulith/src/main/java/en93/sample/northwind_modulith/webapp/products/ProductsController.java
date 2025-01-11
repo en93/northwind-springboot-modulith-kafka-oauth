d887421a0f221086fb00fc98b78b3bcb2ac47c33
Ian Babington <IanBabington93@gmail.com>
2025-01-11 14:16:19 +1300

    generated db classes and using jooq to query postgres

--- northwind-modulith/src/main/java/en93/sample/northwind_modulith/webapp/products/ProductsController.java
+++ northwind-modulith/src/main/java/en93/sample/northwind_modulith/webapp/products/ProductsController.java
@@ -25,38 +25,39 @@ public class ProductsController implements ProductsApi {
 
     @Override
     public ResponseEntity<List<Product>> getProducts(String productKey, String searchProductName) {
-        Category sampleCategory = new Category();
-        sampleCategory.setCategoryId(1);
-        sampleCategory.setDescription("Category description");
-        sampleCategory.setCategoryName("Category name");
+        List<Product> products = productsService.getProducts(productKey, searchProductName);
+//        Category sampleCategory = new Category();
+//        sampleCategory.setCategoryId(1);
+//        sampleCategory.setDescription("Category description");
+//        sampleCategory.setCategoryName("Category name");
+//
+//        Product sampleProduct = new Product();
+//        sampleProduct.setCategory(sampleCategory);
+//        sampleProduct.setName("prod1");
+//        sampleProduct.setDescription("desc1");
+//        sampleProduct.setPrice(5f);
+//        sampleProduct.setSupplierId(2);
+//        sampleProduct.setProductKey("key");
+//        sampleProduct.setUnit("shelf");
+//
+//        Product sampleProduct2 = new Product();
+//        sampleProduct2.setCategory(sampleCategory);
+//        sampleProduct2.setName("test");
+//        sampleProduct2.setDescription("desc2");
+//        sampleProduct2.setPrice(7f);
+//        sampleProduct2.setSupplierId(2);
+//        sampleProduct2.setProductKey("key2");
+//        sampleProduct2.setUnit("box");
+//
+//        Product sampleProduct3 = new Product();
+//        sampleProduct3.setCategory(null);
+//        sampleProduct3.setName("test");
+//        sampleProduct3.setDescription("desc2");
+//        sampleProduct3.setPrice(7f);
+//        sampleProduct3.setSupplierId(2);
+//        sampleProduct3.setProductKey("key3");
+//        sampleProduct3.setUnit("box");
 
-        Product sampleProduct = new Product();
-        sampleProduct.setCategory(sampleCategory);
-        sampleProduct.setName("prod1");
-        sampleProduct.setDescription("desc1");
-        sampleProduct.setPrice(5f);
-        sampleProduct.setSupplierId(2);
-        sampleProduct.setProductKey("key");
-        sampleProduct.setUnit("shelf");
-
-        Product sampleProduct2 = new Product();
-        sampleProduct2.setCategory(sampleCategory);
-        sampleProduct2.setName("test");
-        sampleProduct2.setDescription("desc2");
-        sampleProduct2.setPrice(7f);
-        sampleProduct2.setSupplierId(2);
-        sampleProduct2.setProductKey("key2");
-        sampleProduct2.setUnit("box");
-
-        Product sampleProduct3 = new Product();
-        sampleProduct3.setCategory(null);
-        sampleProduct3.setName("test");
-        sampleProduct3.setDescription("desc2");
-        sampleProduct3.setPrice(7f);
-        sampleProduct3.setSupplierId(2);
-        sampleProduct3.setProductKey("key3");
-        sampleProduct3.setUnit("box");
-
-        return ResponseEntity.ok(List.of(sampleProduct, sampleProduct2, sampleProduct3));
+        return ResponseEntity.ok(products);
     }
 }
--- northwind-modulith/src/main/java/en93/sample/northwind_modulith/webapp/products/ProductsService.java
+++ northwind-modulith/src/main/java/en93/sample/northwind_modulith/webapp/products/ProductsService.java
@@ -4,11 +4,31 @@ import en93.sample.northwind_modulith.generated.webapp.model.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ProductsService {
-    protected ResponseEntity<List<Product>> getProducts(String searchName, Integer supplierId, Integer categoryId, Float minPrice, Float maxPrice, Integer page, Integer limit, String sort, String direction) {
-        return null;
+    private final ProductsRepository repository;
+
+    public ProductsService(ProductsRepository repository) {
+        this.repository = repository;
+    }
+
+    protected List<Product> getProducts(String productKey, String searchProductName) {
+        return repository.findProducts(productKey, searchProductName).stream()
+                .map(entity -> {
+                    //Is there an easier way to map? Externalise to own lib
+                    final Product product = new Product();
+                    product.setProductKey("" + entity.getProductid());
+                    product.setSupplierId(entity.getSupplierid());
+                    product.setUnit(entity.getUnit());
+                    product.setName(entity.getProductname());
+                    product.setCategory(null);//todo
+                    product.setPrice(entity.getPrice().floatValue());
+                    return product;
+                })
+                .collect(Collectors.toList());
     }
 }
