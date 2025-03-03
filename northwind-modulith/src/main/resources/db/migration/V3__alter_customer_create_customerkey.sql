79f509a6004e3addb87c2a75c7d2d05ab0ec9013
Ian Babington <IanBabington93@gmail.com>
2025-03-04 08:43:07 +1300

    creating surrogate key for customer

---
+++ northwind-modulith/src/main/resources/db/migration/V3__alter_customer_create_customerkey.sql
@@ -0,0 +1,17 @@
+--Make idempotent if rerun, clear previous data back to initial state
+ALTER TABLE customers
+DROP COLUMN IF EXISTS customerkey;
+
+--New surrogate key for keys exposed outside of application
+ALTER TABLE customers
+ADD COLUMN customerkey TEXT;
+
+--Set values for existing customers
+UPDATE customers
+SET customerkey = 'ORD-' || uuid_generate_v4()
+WHERE customerkey IS NULL;
+
+--Require not null and unique
+ALTER TABLE customers
+ALTER COLUMN customerkey SET NOT NULL;
+ADD CONSTRAINT unique_customer_key UNIQUE (customerkey);
